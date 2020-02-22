56ebab3721f0610a1f77218b40655ace2cf0f409
kellycoles <kellycoles1@gmail.com>
2020-02-22 10:12:45 -0600

    placeholder text displaying

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import './index.css'
+
+const App = () => {
+    return (
+        <>
+            <input type="checkbox" />
+            <p>placeholder text</p>
+
+            <input type="checkbox" />
+            <p>placeholder text</p>
+
+            <input type="checkbox" />
+            <p>placeholder text</p>
+        </>
+    )
+}
+export default App
--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from 'react'
+import ReactDom from 'react-dom'
+import App from './App'
+import './index.css'
+
+ReactDom.render(
+    <App />, document.getElementById('root')
+)
+
