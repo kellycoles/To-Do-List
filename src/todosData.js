6508a062406169bb39a9094e6db8e865a34f219e
kellycoles <kellycoles1@gmail.com>
2020-03-14 15:22:22 -0500

    create todosData

---
+++ src/todosData.js
@@ -0,0 +1,33 @@
+const todosData = [
+    {
+        id: 1,
+        text: "Grocery store",
+        completed: true
+    },
+    {
+        id: 2,
+        text: "Gym",
+        completed: false
+    },
+    {
+        id: 3,
+        text: "Dump",
+        completed: false
+    },
+    {
+        id: 4,
+        text: "Homework",
+        completed: true
+    },
+    {
+        id: 5,
+        text: "Clean the house",
+        completed: true
+    },
+    {
+        id: 6,
+        text: "Work on the lawnmower",
+        completed: false
+    },
+]
+export default todosData
