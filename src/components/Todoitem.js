5310cabe8e79d0cb5e69c315d26e691921fa8de3
kellycoles <kellycoles1@gmail.com>
2020-03-15 10:26:01 -0500

    event handeling for checked- test

--- src/components/Todoitem.js
+++ src/components/Todoitem.js
@@ -4,8 +4,12 @@ import React from "react"
 const TodoItem = (props) => {
     return (
         <div className="todo-item">
-            <input type="checkbox" />
-    <p>{props.item.text}</p>
+            <input
+                type="checkbox"
+                checked={props.item.completed}
+                onChange={() => console.log("Changed!")}
+            />
+            <p>{props.item.text}</p>
         </div>
     )
 }
